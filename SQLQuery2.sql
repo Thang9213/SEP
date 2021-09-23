20a52278e072fdc728be6e4face954c3a892fe17
Thang9213 <50280344+Thang9213@users.noreply.github.com>
2021-09-23 09:38:52 -0400

    Query 2

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -57,19 +57,10 @@ Select cr.Name as Country, sp.Name as Province
 From Person.CountryRegion cr join Person.StateProvince sp on cr.CountryRegionCode = sp.CountryRegionCode
 where cr.Name in ('Germany','Canada');
 --14
-select *
-From Orders
 
-select *
-From [Order Details]
-
-select *
+select distinct ProductName
 From Orders o inner join [Order Details] od on o.OrderID = od.OrderID inner join Products p on od.ProductID = p.ProductID
-group by ProductName
-
-select *
-From Orders o inner join [Order Details] od on o.OrderID = od.OrderID inner join Products p on od.ProductID = p.ProductID
-group by ProductName
+where OrderDate > '1996-09-23'
 
 --15
 Select top 5 ShipPostalCode
@@ -82,7 +73,7 @@ order by COUNT(*) DESC
 
 Select top 5 ShipPostalCode
 from Orders
-where ShipPostalCode is not null and 
+where ShipPostalCode is not null and OrderDate > '1996-09-23'
 Group by ShipPostalCode
 order by COUNT(*) DESC
 
@@ -105,20 +96,58 @@ group by City
 ) cc
 where cc.C >= 2
 --19
-
+Select distinct c.ContactName 
+from Orders o inner join Customers c on o.CustomerID = c.CustomerID
+where o.OrderDate >= '1998-01-01'
 --20
+
+select distinct co.ContactName
+From (
+Select top 100 c.ContactName 
+from Orders o inner join Customers c on o.CustomerID = c.CustomerID
+order by o.OrderDate Desc
+) co
+
 --21
+
+Select c.ContactName , ROUND(SUM(od.Quantity),2) as [Number of Products]
+from Orders o inner join Customers c on o.CustomerID = c.CustomerID inner join [Order Details] od on o.OrderID = od.OrderID
+Group by c.ContactName
+
 --22
+
+Select c.CustomerID , ROUND(SUM(od.Quantity),2) as [Number of Products]
+from Orders o inner join Customers c on o.CustomerID = c.CustomerID inner join [Order Details] od on o.OrderID = od.OrderID
+group by c.CustomerID
+having ROUND(SUM(od.Quantity),2) > 100
+
 --23
+
+select distinct sp.CompanyName as 'Supplier Company Name', sh.CompanyName as 'Shipping Company Name'
+From Orders o inner join [Order Details] od on o.OrderID = od.OrderID inner join Products p on od.ProductID = p.ProductID 
+inner join Suppliers sp on sp.SupplierID = p.SupplierID inner join Shippers sh on o.ShipVia = sh.ShipperID
+
 --24
+
+select distinct o.OrderDate, p.ProductName
+From Orders o inner join [Order Details] od on o.OrderID = od.OrderID inner join Products p on od.ProductID = p.ProductID
+order by o.OrderDate
+
 --25
 Select e1.FirstName + ' ' + e1.LastName as Firstperson, e2.FirstName + ' ' + e2.LastName as Secoundperson, e1.Title
 From Employees e1 Inner join Employees e2 on e1.Title = e2.Title
+where not e1.FirstName + ' ' + e1.LastName = e2.FirstName + ' ' + e2.LastName
 --26
-Select FirstName + ' ' + LastName as FullName, ReportsTo, EmployeeID
-From Employees
 
-Select distinct e2.FirstName + ' ' + e2.LastName as FullName
+Select e2.FirstName + ' ' + e2.LastName as FullName
 From Employees e1 Inner Join Employees e2 on e1.ReportsTo = e2.EmployeeID
-where 
+group by e2.FirstName + ' ' + e2.LastName
+having count(*) > 2
+
 --27
+
+select City, CompanyName ,ContactName, 'Customer' as 'Type'
+from Customers
+union
+select City, CompanyName, ContactName, 'Supplier'
+from Suppliers
